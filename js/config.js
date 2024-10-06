8c91f941c8ff8e32ac35c9e6f9c92196a1a304f7
hudegit <ah_taurus@hotmail.com>
2024-10-06 19:06:51 +0300

    lisätty gitingore API avaimen config tiedostoa varten

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_KEY: 'sinun_api_avaimesi_tähän'
+  };
+  
+  export default config;
--- js/funtion.js
+++ js/funtion.js
@@ -1,6 +1,6 @@
-const url ='https://api.openweathermap.org/data/3.0/onecall?'
+const url ='https://api.openweathermap.org/data/2.5/weather?'
 const icon_url = 'http://openweathermap.org/img/wn/'
-const api_key = 'be97ea14163186e1ad75588772b58ae7'
+const api_key = ''
 const temp_span = document.querySelector('#temp')
 const speed_span = document.querySelector('#speed')
 const direction_span = document.querySelector('#direction')
